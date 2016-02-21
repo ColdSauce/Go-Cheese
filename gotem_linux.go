d7f88e53a4db96753da08ff26ebe0f7926b40f0e
Stefan Aleksic <stefan@stefanaleksic.com>
2016-02-21 00:54:39 -0500

    Fix compile time bugs

--- gotem_linux.go
+++ gotem_linux.go
@@ -8,17 +8,17 @@ import "C"
 var( dpy = C.XOpenDisplay(nil))
 
 // MoveMouseTo (X Window System version) moves the user's mouse to a specified point on the screen.
-func MoveMouseTo(p Point) error{
+func MoveMouseToL(p Point) error{
 	// TODO: Implement this.
 }
 
 // GetMousePosition (X Window System version) returns a Point indicating where the cursor is located on the screen.
-func GetMousePosition() (Point, error){
+func GetMousePositionL() (Point, error){
 	// TODO: Implement this.
 	return Point{0, 0}
 }
 
 // PressKey (X Window System version) simulates a key press.
-func PressKey(key uintptr, isUp bool) error {
+func PressKeyL(key uintptr, isUp bool) error {
 	// TODO: Implement this.
 }
